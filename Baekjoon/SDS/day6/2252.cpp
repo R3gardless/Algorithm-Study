77a49c77a0af589cab834e0d10f3daa26e99949a
R3gardless <pidaoh@g.skku.edu>
2023-08-04 23:00:29 +0900

    [ADD] SDS 문제풀이

--- Baekjoon/SDS/day6/2252.cpp
+++ Baekjoon/SDS/day6/2252.cpp
@@ -2,33 +2,48 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
 using namespace std;
 
+const int MAX = 33333;
+
 int N, M;
-int a, b;
-vector<pair<int, int> > vec;
+vector<int> arr[MAX];
+// indegree[i]: i 학생 앞에 와야 하는 학생 수
+int indegree[MAX];
+queue<int> myQueue;
+vector<int> ans;
 
 int main() {
-
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
 
     cin >> N >> M;
-
-    vec.assign(N + 1, make_pair(0, 0));
-
-    while(M--) {
-        cin >> a >> b;
-        vec[b].first++;
-        vec[a].second = b;
+    for (int i = 0; i < M; ++i) {
+        int next, pre;
+        cin >> pre >> next;
+        // 뒤 학생의 indgree++
+        indegree[next] += 1;
+        // 앞 학생의 자식 추가
+        arr[pre].push_back(next);
     }
-
-    
-
-    for(int i = 1; i <= N; ++i) cout << vec[i].second << ' ';
-    cout <<'\n';
+    for (int i = 1; i <= N; ++i) {
+        // 자신의 앞에 올 학생이 없는 학생 선별
+        if (indegree[i] == 0)
+            myQueue.push(i);
+    }
+    while (!myQueue.empty()) {
+        int now = myQueue.front();
+        myQueue.pop();
+        ans.push_back(now);
+        for (auto iter : arr[now]) {
+            indegree[iter]--;
+            // now 자식들 중 now 가 빠지면서 indegree=0 이 되는 학생 선별
+            if (indegree[iter] == 0) myQueue.push(iter);
+        }
+    }
+    for (int i = 0; i < N; ++i)
+        cout << ans[i] << ' ';
+    cout << '\n';
 
     return 0;
-
 }
